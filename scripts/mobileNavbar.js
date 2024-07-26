c40be9617b1ff5f1ff48bd930bc5c98a855c7ade
Vinicius <vfarias.designer@gmail.com>
2024-07-25 21:28:30 -0300

    create menu mobile

--- scripts/mobileNavbar.js
+++ scripts/mobileNavbar.js
@@ -1,6 +1,21 @@
+
+const handleClick = (mobileMenu, navItens, navLinks) => {
+    navItens.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+
+    navLinks.forEach(link => {
+        link.style.animation = `navLinkFade 0.5s ease fowards 0.3s`
+
+        console.log(link.style)
+    });
+
+}
+
 export const createMobileMenu = () => {
 
     const mobileMenu = document.querySelector('.mobile-menu');
+    const navItens = document.querySelector('.nav-itens');
+    const navLinks = document.querySelectorAll('.nav-itens li')
     
-    mobileMenu.addEventListener('click', ()=>{console.log('aqui')})
-}
+    mobileMenu.addEventListener('click',() => handleClick(mobileMenu, navItens, navLinks));
+};
